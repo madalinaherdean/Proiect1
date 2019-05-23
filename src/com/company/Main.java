c7b15db8296932d7f301172e557b69326717f4cd
Alina <madalin_herdean@yahoo.com>
2019-05-23 20:45:40 +0300

    initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(2);
+        System.out.println("helloo Mada");
+
+        char myChar = 'a';
+        byte myByte = 123;
+        short myShort = 12312;
+        int myInt = 1;
+        long myLong = 123445567788l;
+        float myFloat = 112312;
+        String myString = "Myname";
+
+        int result = myByte + 2;
+
+                System.out.println("Results is:" + 2+2+3+(2+2));
+
+    }
+}
